ff6cb8ee64c8bfec8f39eedaa55de5baa70e83ec
jenniferjoseph2358 <19jjoseph@gmail.com>
2024-06-10 22:42:39 -0400

    Update set_up_for_stg_models.sql
    add in nested receipt item list

--- set_up_for_stg_models.sql
+++ set_up_for_stg_models.sql
@@ -73,3 +73,20 @@ order by 1,2;
 -- NEW VALUE TYPE --> rewardsReceiptItemList needs to be flattened as well 
 
 -- Bonus #4: Receipt Item List 
+-- notice the slight difference for input 
+with a as (
+    select value as rewardsReceiptItemList_item
+    from receipts,
+     lateral flatten(input => PARSE_JSON(json):rewardsReceiptItemList) AS f
+) 
+select  
+    distinct
+    key AS nested_key,
+    case
+        when IS_OBJECT(value) then 'nested_json_object'
+        when IS_ARRAY(value) then 'nested_json_array'
+        else 'primitive_value'
+    end as value_type,
+from a, 
+    lateral flatten(input => parse_json(rewardsReceiptItemList_item))
+order by 1;
